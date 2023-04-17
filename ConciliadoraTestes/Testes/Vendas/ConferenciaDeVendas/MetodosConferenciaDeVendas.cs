4ae883c8ce2efb64d1cd2acc216588f1fb9da433
leticia.santos <leticia.santos@conciliadora.com.br>
2023-04-17 17:31:25 -0300

    Correção Conferencia de vendas

--- ConciliadoraTestes/Testes/Vendas/ConferenciaDeVendas/MetodosConferenciaDeVendas.cs
+++ ConciliadoraTestes/Testes/Vendas/ConferenciaDeVendas/MetodosConferenciaDeVendas.cs
@@ -19,14 +19,14 @@ namespace ConciliadoraTestes.Testes.Vendas.ConferenciaDeVendas
             IWebElement barra = inicializaDriver.ObterDriver().FindElement(By.XPath("//div[@id='kt_aside_menu']")); // o id foi pego através de copiar o XPATH.
             acao.MoveToElement(barra).Perform();  // usa-se o moveToElement para mover o ponteiro para o elemento e utiliza-se o Perform para executar a ação
 
-            IWebElement dashboard = inicializaDriver.ObterDriver().FindElement(By.XPath("//i[@class='kt-menu__link-icon menuSideIcon fas fa-search-dollar']")); // Localiza o ícone de pagamentos e clica
-            dashboard.Click();
+            IWebElement vendas = inicializaDriver.ObterDriver().FindElement(By.XPath("//i[@class='kt-menu__link-icon menuSideIcon fas fa-search-dollar']")); // Localiza o ícone de pagamentos e clica
+            vendas.Click();
 
             inicializaDriver.ObterDriver().Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
 
-            IWebElement gerencial = inicializaDriver.ObterDriver().FindElement(By.XPath("//span[text()='Conferência de Vendas']")); //Localiza pelo texto exibido
+            IWebElement conferenciaDeVendas = inicializaDriver.ObterDriver().FindElement(By.XPath("//span[text()='Conferência de Vendas']")); //Localiza pelo texto exibido
             inicializaDriver.ObterDriver().Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            gerencial.Click();
+            conferenciaDeVendas.Click();
         }
 
     }
