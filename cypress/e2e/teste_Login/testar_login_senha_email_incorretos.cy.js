744582c99eb27c9c95e22364f8ea3812c0f6bb75
Vitor Ibraim de Paula Reis <vitor.reis@conciliadora.com.br>
2024-05-02 13:57:22 +0000

    Updated testar_login_senha_email_incorretos.cy.js

--- cypress/e2e/teste_Login/testar_login_senha_email_incorretos.cy.js
+++ cypress/e2e/teste_Login/testar_login_senha_email_incorretos.cy.js
@@ -2,6 +2,7 @@ describe('Teste de login com senha e email incorretos, logo em seguida inserir l
   it('Email e Senha incorretos', () => {
  
     //Test in url: [https://app-qas.conciliadora.com.br/] .
+    
     cy.realizarLoginIncorreto("https://app-qas.conciliadora.com.br/");
     cy.validarPopupErro();
     cy.selecionarOKemPopupErro();
