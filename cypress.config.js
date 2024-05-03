dba702d279208924eb5dadee10915a5b47572c81
S41T4M4 <ibraim99.5@gmail.com>
2024-05-03 14:26:24 -0300

    defaultCommandTimeout change

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    defaultCommandTimeout: 15000,
+    defaultCommandTimeout: 25000,
     baseUrl: "https://app-qas.conciliadora.com.br/",
     
     setupNodeEvents(on, config) {
