8f7723894ad67e3f08898f21ef11c993d298d7b9
S41T4M4 <ibraim99.5@gmail.com>
2024-05-31 08:39:04 -0300

    Criado novo teste em [https://intranet-hom.conciliadora.com.br]

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,9 +2,9 @@ import '@4tw/cypress-drag-drop';
 
 
 
-Cypress.Commands.add('login',(url,email,senha)=>{
-  const fomartodeImagem =cy.viewport(1920, 1080);
-  fomartodeImagem(); 
+
+Cypress.Commands.add('logar',(url,email,senha)=>{
+  cy.viewport(1920, 1080);
   cy.visit(url);
   cy.get('#login').type(email);
   cy.get('#password').type(senha);
